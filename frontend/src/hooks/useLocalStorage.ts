fa8f870ceb48b78e40670cbefa02870dfe72faf5
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-12-17 01:38:09 +0100

    chore(Root): Disable any explicit type warning

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -3,6 +3,7 @@ export function useLocalStorage() {
     return localStorage.getItem(key)
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const setItem = ({ key, value }: { key: string; value: any }) =>
     window.localStorage.setItem(key, value)
 
